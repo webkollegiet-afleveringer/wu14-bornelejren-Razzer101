fd7ef6b5b2c5bce60defd8fd29fa66d96829d762
Razzer101 <Rasmus@anders-net.dk>
2026-04-16 09:35:04 +0200

    chore: Router implimentation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { BrowserRouter, Route, Routes } from 'react-router'
-import Home from './components/Home'
+import Home from './pages/Home'
+import AboutUs from './pages/AboutUs'
+import Sponsor from './pages/Sponsor'
+import Thanks from './pages/Thanks'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<Home/>}/>
+        <Route path='/omos' element={<AboutUs/>} />
+        <Route path='/sponsor' element={<Sponsor/>} />
+        <Route path='/thanks' element={<Thanks/>} />
       </Routes>
     </BrowserRouter>
   )
