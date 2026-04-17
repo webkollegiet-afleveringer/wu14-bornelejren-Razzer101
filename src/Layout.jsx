93d80f87a47b3eef0d6ef566cd3bd96b288c1ac0
Razzer101 <Rasmus@anders-net.dk>
2026-04-17 08:42:35 +0200

    refactor: Changed header styling

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -5,7 +5,7 @@ import Nav from "./components/Nav";
 export default function Layout(){
     return(
         <>
-            <header className="border-b-2 border-main flex justify-between">
+            <header className="flex bg-main justify-between">
                 <Link to={"/"} className="w-45 p-2">
                     <img src="/logo.png" alt="Logo" className="w-full rounded-sm"/>
                 </Link>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,8 +7,8 @@ export default function Nav(){
     const [menuDisplay, setMenuDisplay] = useState(false)
 
     return(
-        <nav className="flex items-center pr-[2vw] mb-1">
-            <ul className="gap-7 hidden sm:flex">
+        <nav className="flex items-center  pr-[2vw] mb-1">
+            <ul className="gap-7 hidden sm:flex text-white">
                 <li className="text-2xl font-semibold hover:underline">
                     <Link to={"/"}>Home</Link>
                 </li>
@@ -22,11 +22,11 @@ export default function Nav(){
                     <Link to={"/takker"}>Vi takker</Link>
                 </li>
             </ul>
-            <button onClick={() => setMenuDisplay(menuDisplay ? false : true)} className="sm:hidden text-5xl border-2 p-1 rounded-sm">
+            <button onClick={() => setMenuDisplay(menuDisplay ? false : true)} className="sm:hidden text-5xl text-white border-3 p-1 rounded-sm">
                 <IoMenu className={`${menuDisplay ? "hidden" : ""}`} />
                 <IoClose className={`${menuDisplay ? "" : "hidden"}`}/>
             </button>
-            <ul className={`${menuDisplay ? "flex" : "hidden"} flex-col items-center sm:hidden h-[calc(100%-89px)] gap-5 py-5 w-[50%] bg-white border-l-2 border-main absolute top-22.25 right-0`}>
+            <ul className={`${menuDisplay ? "flex" : "hidden"} flex-col items-center sm:hidden h-[calc(100%-87px)] gap-5 py-5 w-[50%] bg-white border-l-3 border-main absolute top-21.75 right-0 z-5`}>
                 <li className="text-2xl font-semibold hover:underline">
                     <Link to={"/"}>Home</Link>
                 </li>
