f4f7bd77b1346ced2afdcd6f163d50474228ca23
Razzer101 <Rasmus@anders-net.dk>
2026-04-20 12:42:59 +0200

    feat: Thanks.jsx base build complete

--- src/pages/Thanks.jsx
+++ src/pages/Thanks.jsx
@@ -1,7 +1,30 @@
 export default function Thanks(){
     return(
         <>
-            <h1>Vi Takker</h1>
+            <h1 className="text-center text-4xl font-bold my-3">Børnelejren takker</h1>
+            <p className="mb-15 sm:mb-30 px-4 sm:px-20 text-xl">
+                Børnelejren på Langeland takker <b>alle</b>, der på den ene eller anden måde, har støttet
+                foreningens arbejde med at sende dårligt stillede børn på et ophold på Søgård Hovedgård
+                - det være sig ved naturaliesponsorater eller økonomisk støtte fra støttemedlemmer,
+                virksomhedssponsorer og donationer fra fonde.
+            </p>
+            <section className="text-center text-2xl font-bold my-3">
+                <h2>En særlig tak til:</h2>
+                <ul className="font-normal text-xl">
+                    <li>
+                        <p>Navn:</p>
+                    </li>
+                    <li>
+                        <p>Navn:</p>
+                    </li>
+                    <li>
+                        <p>Navn:</p>
+                    </li>
+                    <li>
+                        <p>Navn:</p>
+                    </li>
+                </ul>
+            </section>
         </>
     )
 }
